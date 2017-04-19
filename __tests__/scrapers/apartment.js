d322be1f3e82ce3de3784c04e3c872095947b5fd
ahmed ayoub <ahmed.mahmoud@zalando.de>
2017-04-20 00:18:33 +0200

    add apartment photos

--- __tests__/scrapers/apartment.js
+++ __tests__/scrapers/apartment.js
@@ -1,28 +1,38 @@
-const fs = require('fs');
-const scraper = require('../../src/scrapers/apartment');
+const fs      = require( 'fs' );
+const path    = require( 'path' );
+const scraper = require( '../../src/scrapers/apartment' );
 
-it('scraps the apartment1 page', () => {
-    const file = fs.readFileSync(`${process.cwd()}/__tests__/scrapers/pages/apartment1.html`);
-    const apartment = scraper.scrap(file);
+it( 'scraps the apartment1 page', () => {
+  const file      = fs.readFileSync( `${process.cwd()}/__tests__/scrapers/pages/apartment1.html` );
+  const apartment = scraper.scrap( file );
 
-    expect(apartment.description)
-        .toBe('Erstbezug nach Modernisierung mit neuer offener Einbauküche und neuem Duschbad');
+  expect( apartment.description )
+    .toBe( 'Erstbezug nach Modernisierung mit neuer offener Einbauküche und neuem Duschbad' );
 
-    expect(apartment.area).toBe(28.8);
-    expect(apartment.availableFrom).toEqual(new Date('2017-02-16'));
-    expect(apartment.rentTotal).toBe(720);
-    expect(apartment.rentBase).toBe(650);
-});
+  expect( apartment.area ).toBe( 28.8 );
+  expect( apartment.availableFrom ).toEqual( new Date( '2017-02-16' ) );
+  expect( apartment.rentTotal ).toBe( 720 );
+  expect( apartment.rentBase ).toBe( 650 );
+} );
 
-it('scraps the apartment2 page', () => {
-    const file = fs.readFileSync(`${process.cwd()}/__tests__/scrapers/pages/apartment2.html`);
-    const apartment = scraper.scrap(file);
+it( 'scraps the apartment2 page', () => {
+  const file      = fs.readFileSync( `${process.cwd()}/__tests__/scrapers/pages/apartment2.html` );
+  const apartment = scraper.scrap( file );
 
-    expect(apartment.description).toBe('Erstbezug nach umfassender Modernisierung!');
+  expect( apartment.description ).toBe( 'Erstbezug nach umfassender Modernisierung!' );
 
-    expect(apartment.area).toBe(76.93);
-    expect(apartment.availableFrom).toEqual(new Date('2017-02-01'));
-    expect(apartment.rentTotal).toBe(1350);
-    expect(apartment.rentBase).toBe(1150);
-    expect(apartment.rooms).toBe(2);
-});
+  expect( apartment.area ).toBe( 76.93 );
+  expect( apartment.availableFrom ).toEqual( new Date( '2017-02-01' ) );
+  expect( apartment.rentTotal ).toBe( 1350 );
+  expect( apartment.rentBase ).toBe( 1150 );
+  expect( apartment.rooms ).toBe( 2 );
+} );
+
+it( 'scraps the apartment photos page', () => {
+  const file      = fs.readFileSync( path.resolve('__tests__', 'scrapers', 'pages', 'fotos.html') );
+  const apartment = scraper.scrapPhotos( file );
+
+  expect( apartment.photos.length ).toBe( 11 );
+  expect( apartment.photos ).toContain('http://media-assets.akelius.com/fotoweb/cache/5013/Assets/4017/0019/4017_0019_-_-_2017-04-10_-_Marketing_-_06340170019-4017-0019-02-sfl-eg-rechts_lettings.t58eb600c.m600.x44f09ca4.jpg');
+
+} );
--- src/index.js
+++ src/index.js
@@ -1,47 +1,63 @@
-const request = require('request');
-const listScraper = require('./scrapers/list');
-const apartmentScraper = require('./scrapers/apartment');
-const cityPaths = require('./cityPaths.json');
+const request          = require( 'request' );
+const listScraper      = require( './scrapers/list' );
+const apartmentScraper = require( './scrapers/apartment' );
+const cityPaths        = require( './cityPaths.json' );
+const host             = 'https://www.akelius.de';
+const basePathname     = '/en/search/apartments/';
+const photosPageName   = 'fotos'
 
-const host = 'https://www.akelius.de';
-const basePathname = '/en/search/apartments/';
+const requestPromise = url => {
+  return new Promise( ( resolve, reject ) => {
+    request( url, ( error, response, body ) => {
 
-const scrapApartment = (apartment) => new Promise((resolve, reject) => {
-    const url = host + apartment.url;
-    request(url, (error, response, body) => {
-        if (error) {
-            reject(error);
-        }
-        if (response.statusCode !== 200) {
-            reject(`Invalid response: ${response}`);
-        }
-        const apartmentInfo = apartmentScraper.scrap(body);
-        apartment = Object.assign(apartment, apartmentInfo);
-        apartment.url = url;
+      if ( error ) reject( error );
+      if ( response.statusCode !== 200 ) reject( `Invalid response: ${response}` );
 
-        resolve(apartment);
-    });
-});
+      resolve( body );
+    } );
+  } )
+}
 
-const scrapCity = city => new Promise((resolve, reject) => {
-    const cityPath = cityPaths[city];
-    if (!cityPath) {
-        reject(`Invalid city: ${city}`);
+const scrapApartment = ( apartment, includePhotos = true ) => {
+  const url               = host + apartment.url;
+  const photosPageUrl     = `${url}/${photosPageName}`;
+  const apartmentPromises = [];
+
+  apartmentPromises.push( requestPromise( url ) );
+
+  if ( includePhotos ) {
+    apartmentPromises.push( requestPromise( photosPageUrl ) );
+  }
+
+  return Promise
+    .all( apartmentPromises )
+    .then( ( [ infoPage, photosPage ] ) => {
+      const info   = apartmentScraper.scrap( infoPage );
+      const photos = includePhotos ? apartmentScraper.scrapPhotos( photosPage ) : { photosPageUrl };
+
+      return Object.assign( apartment, { url }, info, photos );
+    } );
+};
+
+const scrapCity = ( city, includePhotos ) => new Promise( ( resolve, reject ) => {
+  const cityPath = cityPaths[ city ];
+  if ( !cityPath ) {
+    reject( `Invalid city: ${city}` );
+  }
+  const url = `${host}${basePathname}${cityPath}/list`;
+
+  request( url, ( error, response, body ) => {
+    if ( error ) {
+      reject( error );
     }
-    const url = `${host}${basePathname}${cityPath}/list`;
+    if ( response.statusCode !== 200 ) {
+      reject( `Invalid response: ${response}` );
+    }
+    const apartments    = listScraper.scrap( body );
+    const scrapPromises = apartments.map( apartment => scrapApartment( apartment, includePhotos ) );
+    resolve( Promise.all( scrapPromises ) );
+  } );
+} );
 
-    request(url, (error, response, body) => {
-        if (error) {
-            reject(error);
-        }
-        if (response.statusCode !== 200) {
-            reject(`Invalid response: ${response}`);
-        }
-        const apartments = listScraper.scrap(body);
-        const scrapPromises = apartments.map(apartment => scrapApartment(apartment));
-        resolve(Promise.all(scrapPromises));
-    });
-});
-
-exports.cities = Object.keys(cityPaths);
+exports.cities    = Object.keys( cityPaths );
 exports.scrapCity = scrapCity;
